5c09c2ce461a19bc4f14cfec55e3cba9a93df959
chakri8826 <kommuchakri8826@gmail.com>
2024-12-14 11:38:03 +0530

    0708

---
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect,useState } from 'react'
+import { useLoaderData } from 'react-router-dom';
+
+function Github() {
+    const data = useLoaderData()
+    // const [data,setData] = useState([])
+    // useEffect(()=>{
+    //     fetch("https://api.github.com/users/chakri8826")
+        
+    //       .then((res) => res.json())
+    //       .then((data) => {
+    //         console.log(data);
+    //         setData(data);
+    //       });
+    // },[])
+
+    return (
+        <div className="bg-gray-600 text-orange-600 p-4 m-4 text-3xl">
+            Github followers:{data.followers}
+            <img  src={data.avatar_url} width={300}/>
+             </div>
+    )
+}
+
+export default Github
+
+export const githubInfoLoader = async()=>{
+    const response = await fetch("https://api.github.com/users/chakri8826")
+    return response.json()
+
+}
---
+++ 08miniContext/src/components/Login.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { useState } from 'react'
+import UserContext from '../context/UserContext'
+import { useContext } from 'react'
+
+
+function Login() {
+    const [Username,setUsername]  = useState("")
+    const [password, setpassword] = useState("")
+
+    const {setUser} = useContext(UserContext)
+
+    const handleSubmit = (e)=>{
+        e.preventDefault()
+        setUser({ Username, password });
+    }
+
+
+    return (
+      <div>
+        <h1>Login</h1>
+        <input
+          onChange={(e) => setUsername(e.target.value)}
+          value={Username}
+          type="text"
+          placeholder="Username"
+        />
+        <input
+          onChange={(e) => setpassword(e.target.value)}
+          value={password}
+          type="text"
+          placeholder="Password"
+        />
+        <button onClick={handleSubmit}>Submit</button>
+      </div>
+    );
+}
+
+export default Login
+
+
