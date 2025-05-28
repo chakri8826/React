7c5993d2c48c2b03d72c600c4ddf0c949ca15ea4
chakri8826 <kommuchakri8826@gmail.com>
2025-05-28 07:16:18 +0530

    new

---
+++ CCBP/4RoutingUsingReact/02_ApiCalls/src/App.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Header from "./components/Header";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import BlogsList from "./components/BlogsList";
+import NotFound from "./components/NotFound";
+
+import "./App.css";
+
+const App = () => (
+  <BrowserRouter>
+    <Header />
+    <Routes>
+      <Route path="/" element={<BlogsList />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </BrowserRouter>
+);
+
+export default App;
